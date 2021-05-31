de55e8215acbaf0fd45493293eae66517866e865
Despades <85120710+Despades@users.noreply.github.com>
2021-05-31 21:47:22 +0500

    Update telegramBot.py

--- telegramBot.py
+++ telegramBot.py
@@ -1,10 +1,9 @@
-import telegram
+import telegram #здесь подключаем библиотеку python-telegram-bot
 import os
 
-
+#инициализация нашего бота (предварительно его необходимо настроить в самом телеграмме)
 TOKEN = 'enter_your_token'
 bot = telegram.Bot(token=TOKEN)
-
 
 async def send_detect_foto(filename):
     chat_id = bot.get_updates()[-1].message.chat_id
